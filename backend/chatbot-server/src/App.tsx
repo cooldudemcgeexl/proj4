b57eb542626501743330b2a9867041116215fdc1
Nick Lay <layns@mail.uc.edu>
2023-03-05 21:54:11 -0500

    create react app to host chatbot

---
+++ backend/chatbot-server/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Cloud Computing Project 4 - Chatbot
+        </p>
+        <p>
+          Click the chat button in the botton right corner and type a greeting to begin.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by React 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
